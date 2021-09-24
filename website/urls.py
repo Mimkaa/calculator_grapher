af191835728653fc9f557b58449605ed82e1fb4a
Illia Solohub <you@youraddress.com>
2021-09-24 10:51:50 +0300

    9 comit

--- website/urls.py
+++ website/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('',include('affiliate.urls')),
     path('members/',include('django.contrib.auth.urls')),
     path('members/',include('members.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
